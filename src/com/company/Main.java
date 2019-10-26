55c00949085db96df1226a1e1d3ca47ea84ab2c1
lantzmarlene <lantzmarlene@gmail.com>
2019-10-26 20:34:05 +0200

    Works with Input Data, takes a long time with Unsorteddict data

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,15 @@
 package com.company;
 
 import java.io.*;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
 
+
     public static void main(String[] args) throws IOException {
-        //Access the contents of a file
-        File file = new File("C:\\Users\\Marlene\\Workspace\\LinkedDict\\src\\com\\company\\unsorteddict.txt");
+        System.out.println("Starting");
+        File file = new File("C:\\Users\\Marlene\\Workspace\\LinkedDict\\src\\com\\company\\testInput.txt");
         Scanner scan = new Scanner(file);
 
         //Creation of linked list and line variable
@@ -16,17 +18,36 @@ public class Main {
         int lineNum = 0;
 
         //Writing contents into a file
-        FileWriter writer = new FileWriter("C:\\Users\\Marlene\\Workspace\\LinkedDict\\src\\com\\company\\sorteddict.txt");
+        FileWriter writer = new FileWriter("C:\\Users\\Marlene\\Workspace\\LinkedDict\\src\\com\\company\\testOutput.txt");
 
         while(scan.hasNextLine()){
             fileContents = scan.nextLine();
-            linkedList.add(fileContents);
-            linkedList.head = linkedList.mergeSort(linkedList.head);
             lineNum++;
+            int x = 0;
+            if (linkedList.size() == 0) {
+                linkedList.add(0, fileContents);
+            } else
+                for (x = 0; x < linkedList.size(); x++) {
+                    if (linkedList.get(x).compareToIgnoreCase(fileContents) > 0) {
+                        linkedList.add(x, fileContents);
+                        break;
+                    }
+                }
+            if (x == linkedList.size()) {
+                linkedList.add(fileContents);
+            }
+
+        }
+        for (int i = 0; i < linkedList.size(); i++) {
+            System.out.println(linkedList.get(i));
+            writer.write(linkedList.get(i) + "\n");
         }
 
         //Close Writer
-        writer.write(linkedList.toString());
         writer.close();
+
+        System.out.println("Ended");
     }
+
+
 }
